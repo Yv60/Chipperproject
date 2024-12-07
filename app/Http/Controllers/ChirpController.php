9069b4cace02f402407eed86eb09420092aed57b
ASSONGBA Méchac <mechacassongba68@gmail.com>
2024-12-07 20:28:47 +0100

    Récupération des chirps et connection des utilisateurs aux chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -18,7 +18,11 @@ class ChirpController extends Controller
     {
 
 
-        return view('chirps.index');
+        return view('chirps.index', [
+
+            'chirps' => Chirp::with('user')->latest()->get(),
+
+        ]);
 
     }
 
