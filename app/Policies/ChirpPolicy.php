26f1f3265a6d901156a587ef98d4743acef3382d
ASSONGBA Méchac <mechacassongba68@gmail.com>
2024-12-07 20:48:17 +0100

    Teste des nouvelles fonctionnalités ajoutés qui autorise seul l'auteur d'un chirp de le modifier

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -36,8 +36,13 @@ class ChirpPolicy
      * Determine whether the user can update the model.
      */
     public function update(User $user, Chirp $chirp): bool
+
     {
-        return false;
+
+        //
+
+        return $chirp->user()->is($user);
+
     }
 
     /**
